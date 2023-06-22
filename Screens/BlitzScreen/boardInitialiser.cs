03115b108d5f1e6ac85b8bafe50c2641c643b7c5
cyp1p5 <coy001@e.ntu.edu.sg>
2023-06-22 15:04:21 +0800

    cy w-8 c4.11: fixed previous commit

--- Screens/BlitzScreen/boardInitialiser.cs
+++ Screens/BlitzScreen/boardInitialiser.cs
@@ -72,7 +72,7 @@ namespace WordBlitz.Screens.BlitzScreen
 
         private static void OnGridButtonPanned(object sender, PanUpdatedEventArgs e)
         {
-            if (e.StatusType == GestureStatus.Completed) { return; }
+            if (e.StatusType == GestureStatus.Completed) { Submit.Word(); }
             if (e.StatusType == GestureStatus.Started) { Console.WriteLine("swipe started"); return; }
             if (SwipeLogic.GetPosition(sender, e) != null)
             {
